1b5b89997376ba248a3fb9e3e218d823dc420637
Shubham <shubhamvscode@gmail.com>
2023-07-18 15:12:45 +0530

    added prod url in backend

--- server/index.js
+++ server/index.js
@@ -3,7 +3,7 @@ const app = express();
 const http = require("http").Server(app);
 const io = require("socket.io")(http, {
   cors: {
-    origin: "*",
+    origin: "https://guileless-lebkuchen-42e91f.netlify.app",
   },
 });
 
