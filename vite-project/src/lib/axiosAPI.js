0082e3e95d2e212dea62daab0a3fbcafa9621648
Shubham <shubhamvscode@gmail.com>
2023-07-21 11:46:50 +0530

    updated axios prod url

--- vite-project/src/lib/axiosAPI.js
+++ vite-project/src/lib/axiosAPI.js
@@ -1,6 +1,7 @@
 import axios from "axios";
 
 export const AXIOS_API = axios.create({
-  baseURL: "http://localhost:3000/api",
+  // baseURL: "http://localhost:3000/api",
+  baseURL: "https://google-docs-abot.onrender.com/",
   withCredentials: true,
 });
